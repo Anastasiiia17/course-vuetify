d778227eb799544fa8fa087c0a85147c8f34a09a
Anastasiiia17 <77485993+Anastasiiia17@users.noreply.github.com>
2022-02-14 14:43:36 +0700

    add base files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+      <v-app-bar density="compact" absolute>
+        <v-app-bar-title>HangZen</v-app-bar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn text>Create</v-btn>
+  
+        <v-btn class="mr-3" text>Help</v-btn>
+  
+        <v-divider inset vertical></v-divider>
+  
+        <v-btn
+          class="mr-4 ml-5"
+          color="primary"
+          plain
+        >
+          <v-icon left icon="mdi-handshake-outline"></v-icon>
+  
+          <span>John Leider</span>
+        </v-btn>
+      </v-app-bar>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  
+}
+</script>
